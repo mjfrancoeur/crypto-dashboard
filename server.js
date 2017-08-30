686484868399103d055acd352bdb5d94dae1519d
Michael Francoeur <mjfrancoeur@gmail.com>
2017-08-30 11:20:04 -0400

    Add functional POST routes for signup/login [WIP]
    Allow users to login and signup for the site. Adds new users to the
    database.
    
    Utilizes bcrypt-as-promised to hash/compare passwords. Implements
    body-parser middleware.

--- server.js
+++ server.js
@@ -4,9 +4,19 @@ const express = require('express');
 
 const app = express();
 
+const cookieSession = require('cookie-session');
+
+const bodyParser = require('body-parser');
+
+const bcrypt = require('bcrypt-as-promised');
+
 const PORT = process.env.PORT || 8000;
 
 app.set('view engine', 'ejs');
+
+// parse application/x-www-form-urlencoded 
+// Adds to req.body
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('home');
@@ -14,16 +24,37 @@ app.get('/', (req, res) => {
 
 // Render ./views/login.ejs
 app.get('/login', (req, res) => {
-  res.render('login');
+  res.render('login', { error: null });
 });
 
 // Verify login credentials
 // If valid, redirect to GET /profile
 app.post('/login', (req, res) => {
+  // Grab data from request body (parsed by middleware)
+  const email = req.body.email;
+  const password = req.body.password;
   // Verify login
-  
-  // Redirect to GET /profile/email
-  // res.redirect('/profile/${email});
+  // Return array of users that match given email
+  knex('users').where({ email: email })
+    .then((users) => {
+      users.forEach((user) => {
+        bcrypt.compare(password, user.password_digest)
+          .then(() => {
+            // if password is correct
+            res.redirect(`/profile/${user.first_name}${user.last_name}`);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.render('login', { error: 'Incorrect password.' });
+          });
+      });
+    })
+    .catch((err) => {
+      // If database query fails/returns no users with given email
+      // Reload login page again with error message
+      console.log(err);
+      res.render('/login', { error: 'Couldn\'t find a user with that email address.' });
+    });
 });
 
 // Render ./views/signup.ejs
@@ -35,9 +66,33 @@ app.get('/signup', (req, res) => {
 // Add user entry to user database
 // If valid, redirect to GET /profile
 app.post('/signup', (req, res) => {
-  
-  // If valid, redirect to profile page
-  res.render('/profile/email');
+  // Get data from request body
+  const password = req.body.password;
+  // Hash password with bcrypt
+  bcrypt.hash(password, 10)
+    .then((pwDigest) => {
+      // Attemp to add user info to database
+      knex('users').insert({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        password_digest: pwDigest,
+      })
+        .then(() => {
+          // if table insert completes
+          res.redirect(`/profile/${req.body.first_name}${req.body.last_name}`);
+        })
+        .catch((dbErr) => {
+          // if table insert fails
+          console.log('Error adding user to database');
+          res.send(dbErr).status(500);
+        });
+    })
+    .catch((hashErr) => {
+      // if bcrypt hashing fails
+      console.log('Error with bcrypt hashing of password');
+      res.send(hashErr).status(500);
+    });
 });
 
 // Direct user to their profile page
