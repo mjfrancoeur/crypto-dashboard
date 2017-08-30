e303e58e421601d1dc02339942df4c62811e2275
Michael Francoeur <mjfrancoeur@gmail.com>
2017-08-30 09:58:54 -0400

    Add initial GET and POST routes [WIP]
    Begin fleshing out requests to render the homepage, logging in, and
    signing in.

--- server.js
+++ server.js
@@ -9,30 +9,41 @@ const PORT = process.env.PORT || 8000;
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
+  res.render('home');
 });
 
 // Render ./views/login.ejs
 app.get('/login', (req, res) => {
+  res.render('login');
 });
 
 // Verify login credentials
 // If valid, redirect to GET /profile
 app.post('/login', (req, res) => {
+  // Verify login
+  
+  // Redirect to GET /profile/email
+  // res.redirect('/profile/${email});
 });
 
 // Render ./views/signup.ejs
 app.get('/signup', (req, res) => {
+  res.render('signup');
 });
 
 // Verify that all necessary info has been input
 // Add user entry to user database
 // If valid, redirect to GET /profile
 app.post('/signup', (req, res) => {
+  
+  // If valid, redirect to profile page
+  res.render('/profile/email');
 });
 
 // Direct user to their profile page
 // If user is logged in / authorized
 app.get('/profile/:id', (req, res) => {
+  res.render('profile');
 });
 
 app.listen(PORT, () => {
