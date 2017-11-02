da8b7245ae6f4d0f3e399c8862b093651a708c5a
Michael Francoeur <mjfrancoeur@gmail.com>
2017-11-02 19:51:08 -0400

    Add ORM data model for Currency

---
+++ models/Currency.js
@@ -0,0 +1,26 @@
+class Currency {
+  constructor({currencyName, currencyID}) {
+    this.currencyName = currencyName;
+    this.currencyID = currencyID;
+  }
+
+  // Static Method: Find By Currency Name
+  // ------------------------------------ 
+  // Takes a currency name ("BTC", "ETH", etc)
+  // as an argument and returns a promise that
+  // resolves with the currency ID 
+  static findByCurrencyName(currencyName) {
+    return new Promise((resolve, reject) => {
+    db.select('currency_id').from('currencies').where({ currency_name: currencyName }).first()
+      .then((data) => {
+        let currencyID = data.currency_id;
+        resolve(currencyID);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+    });
+  }
+}
+
+module.exports = Currency;
